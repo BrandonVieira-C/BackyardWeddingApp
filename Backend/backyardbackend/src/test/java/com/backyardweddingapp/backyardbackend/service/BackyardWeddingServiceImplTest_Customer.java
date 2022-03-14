0b8ea792e695fb9b0078b2aec9446c079b682062
unknown <garengh4@gmail.com>
2022-03-14 17:37:37 -0400

    added working unitTesting for service

---
+++ Backend/backyardbackend/src/test/java/com/backyardweddingapp/backyardbackend/service/BackyardWeddingServiceImplTest_Customer.java
@@ -0,0 +1,50 @@
+package com.backyardweddingapp.backyardbackend.service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.backyardweddingapp.dto.CustomerDTO;
+import com.backyardweddingapp.exception.BackyardWeddingException;
+import com.backyardweddingapp.repository.CustomerRepository;
+import com.backyardweddingapp.service.BackyardWeddingService;
+import com.backyardweddingapp.service.BackyardWeddingServiceImpl;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest //TODO: change this to be more specific: WebMVC?
+public class BackyardWeddingServiceImplTest_Customer {
+  
+  @Autowired
+  private CustomerRepository customerRepository;
+
+  @Autowired
+  private BackyardWeddingService customerService = new BackyardWeddingServiceImpl();
+
+  @Test
+  void createValidCustomer() {
+
+    CustomerDTO customerDTO = new CustomerDTO();
+    customerDTO.setEmail("annTseng@gmail.com");
+    customerDTO.setFirstName("Ann");
+    customerDTO.setLastName("Tseng");
+    customerDTO.setCity("Taiwan");
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+    String date = "16/09/1999";
+    LocalDate dob = LocalDate.parse(date, formatter);
+    customerDTO.setDob(dob);
+    
+    try {
+      customerService.addCustomer(customerDTO);
+    } catch (BackyardWeddingException e) {
+      Logger.getLogger(BackyardWeddingServiceImplTest_Customer.class.getName()).log(Level.SEVERE, null, e);
+      Assertions.fail("customer was valid. no exception should have been thrown.");
+    }
+  }
+
+
+}
