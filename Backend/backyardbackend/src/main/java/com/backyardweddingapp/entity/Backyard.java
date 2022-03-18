c8c8ab99e2caeda4e5eae04ad31767f2364be32a
BrandonVieira-C <brandon.c.vieira@gmail.com>
2022-03-18 17:09:03 -0400

    l

--- Backend/backyardbackend/src/main/java/com/backyardweddingapp/entity/Backyard.java
+++ Backend/backyardbackend/src/main/java/com/backyardweddingapp/entity/Backyard.java
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-//HELLO
+//HELLO we in tha building
 @Entity
 public class Backyard {
 	@Id
